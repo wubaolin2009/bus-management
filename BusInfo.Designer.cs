8e21024dd6847ed141f172407ee9194a1fc3c578
wubaolin2009 <wubaolin@wubaolin-virtual-machine.(none)>
2013-12-01 14:49:48 +0800

    My Project for Database designe.
    Simple Bus and tickets management system. Done on June 2008.

---
+++ BusInfo.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace da1
+{
+    partial class BusInfo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonReject = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBoxLineNum = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(1, 59);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(669, 177);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView_UserAddedRow);
+            this.dataGridView.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView_UserDeletingRow);
+            this.dataGridView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellEndEdit);
+            this.dataGridView.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView_DataError);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonOk.Location = new System.Drawing.Point(160, 242);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(100, 25);
+            this.buttonOk.TabIndex = 4;
+            this.buttonOk.Text = "确认更改";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonReject
+            // 
+            this.buttonReject.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonReject.Location = new System.Drawing.Point(410, 242);
+            this.buttonReject.Name = "buttonReject";
+            this.buttonReject.Size = new System.Drawing.Size(100, 25);
+            this.buttonReject.TabIndex = 5;
+            this.buttonReject.Text = "取消更改";
+            this.buttonReject.UseVisualStyleBackColor = true;
+            this.buttonReject.Click += new System.EventHandler(this.buttonReject_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.Location = new System.Drawing.Point(557, 10);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(100, 40);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "关闭";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBoxLineNum
+            // 
+            this.textBoxLineNum.Location = new System.Drawing.Point(89, 12);
+            this.textBoxLineNum.Name = "textBoxLineNum";
+            this.textBoxLineNum.Size = new System.Drawing.Size(100, 21);
+            this.textBoxLineNum.TabIndex = 0;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSearch.Location = new System.Drawing.Point(225, 10);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "浏览";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "线路编号";
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSelect.Location = new System.Drawing.Point(320, 10);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelect.TabIndex = 2;
+            this.buttonSelect.Text = "查找";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BusInfo
+            // 
+            this.AcceptButton = this.buttonSelect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 279);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxLineNum);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonReject);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "BusInfo";
+            this.Text = "BusInfo";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        protected System.Windows.Forms.DataGridView dataGridView;
+        protected System.Windows.Forms.Button buttonOk;
+        protected System.Windows.Forms.Button buttonReject;
+        private System.Windows.Forms.TextBox textBoxLineNum;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
